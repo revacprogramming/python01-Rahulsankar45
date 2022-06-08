6a69eb5a960e7db22e068427302b3f81d9cafe05
Rahulsankar45 <96513391+Rahulsankar45@users.noreply.github.com>
2022-06-08 09:28:39 +0000

    p7

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,35 @@
 # Files
 
 filename = "dataset/mbox-short.txt"
+str1 = "hello"
+str2 = "rahul"
+r = str1 + str2
+print(r)
+fruit = "banana"
+letter = fruit[4]
+print(letter)
+x = 4
+r = fruit[x-1]
+print(r)
+x = "rahul"
+print(len(x))
+fruit = 'reva'
+index = 0
+while index <len(fruit):
+  letter = fruit[index]
+  print(index, letter)
+  index = index +1
+fruit ="banana"
+for letter in fruit :
+  print(letter)
+x = "banana"
+count =0
+for letter in x:
+  if letter == 'a':
+    count = count +1
+print(count)
+s = "rahul jain"
+print(s[0:6])
+a = "hello"
+b = a + "hello"
+print(b)
--- ActivitySet01/problem15.py
+++ ActivitySet01/problem15.py
@@ -1,2 +1,11 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+if a>=b and a>=c:
+  print("a is largest number",a )
+elif b>=a and b>=c:
+  print("b is the largest number",b)
+else:
+  print("c is the largest number ",c)
